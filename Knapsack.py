e87f2e5096a4d4f75dbe974c01759faab2f608cc
DominicVerschoor <91744699+DominicVerschoor@users.noreply.github.com>
2024-02-26 12:56:14 +0100

    Create Knapsack.py

---
+++ Knapsack.py
@@ -0,0 +1,49 @@
+import random
+
+
+def generate_item_list(weight_limit):
+    pop = {}
+    for n in range(1, weight_limit+1):
+        pop[n] = random.randint(1,weight_limit)
+    return pop
+
+def generate_population(item_list, weight_limit, pop_size):
+    pops = []
+    for i in range(pop_size):
+        group = {}
+        keys = random.sample(list(item_list), k=len(item_list))
+        for key in keys:
+            weight = item_list[key]
+            if (sum(group.values()) + weight <= weight_limit):
+                group[key] = weight
+        pops.append(group)
+    return pops
+
+def ranking_population(population):
+
+    sorted_pop = sorted(population, key=lambda d: sum(d.keys()), reverse=True)
+    scores_list = []
+    for group in sorted_pop:
+        scores_list.append(sum(group.keys()))
+    return sorted_pop, scores_list
+
+def tournament_selection(population, k=5):
+    mating_pool = []
+    while (len(mating_pool) != len(population)):
+        group = random.sample(population, k)
+        champion = sorted(group, key=lambda d: sum(d.keys()), reverse=True)[0]
+        mating_pool.append(champion)
+
+    return mating_pool
+
+weight_limit = 10
+item_list = generate_item_list(weight_limit)
+
+pop_size = 100
+population = generate_population(item_list, weight_limit, pop_size)
+reordered_population = ranking_population(population)[0]
+
+pop_pool = tournament_selection(reordered_population)
+
+print(pop_pool)
+
